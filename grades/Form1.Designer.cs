fb50717b997102bf8478ac69fd0f1c9c49da1be6
Bikramjit Singh Mall <BM042@atcltd.atc.org.nz>
2019-01-24 11:57:50 +1300

    Add project files.

---
+++ grades/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace grades
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.lbxGrade = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(80, 53);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(152, 54);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(421, 53);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(100, 20);
+            this.txtGrade.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(421, 110);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(421, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Grades";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Name Here";
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblGrade.Location = new System.Drawing.Point(421, 180);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(100, 23);
+            this.lblGrade.TabIndex = 5;
+            // 
+            // lbxGrade
+            // 
+            this.lbxGrade.FormattingEnabled = true;
+            this.lbxGrade.Location = new System.Drawing.Point(80, 165);
+            this.lbxGrade.MultiColumn = true;
+            this.lbxGrade.Name = "lbxGrade";
+            this.lbxGrade.Size = new System.Drawing.Size(120, 95);
+            this.lbxGrade.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxGrade);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.ListBox lbxGrade;
+    }
+}
+
---
+++ grades/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grades
+{
+    public partial class Form1 : Form
+    {
+        private string Name;
+        //holds text or non numbers        
+        private Single Score;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+           /* Score = Convert.ToSingle(txtGrade.Text);
+            Name = txtName.Text;
+            if
+              (Convert.ToSingle(txtGrade.Text) < 50)
+            {
+                lblGrade.Text = "Fail"; lbxGrade.Items.Add(Name + " " + txtGrade.Text + " Fail");
+            }
+            else
+            {
+                lblGrade.Text = "Pass"; lbxGrade.Items.Add(Name + " " + txtGrade.Text + " Pass");
+            }*/
+            {
+                if (txtGrade.Text == "")
+                {
+                    lblGrade.Text = "Nothing Entered, Nothing Gained";
+                    return;
+                }
+                //Move the grade to the Score
+                Score = Convert.ToSingle(txtGrade.Text);
+                //Move the Name to the Name
+                Name = txtName.Text;
+                //test to check if its true
+                if (Score < 0)
+                {
+                    txtGrade.Text = "";
+                    txtName.Text = "";
+                    lblGrade.Text = Score + " " + Name + " Fail";
+                    lbxGrade.Items.Add(Score + " " + Name + " Fail");
+                }
+                if (Score <= 50)
+                {
+                    lblGrade.Text = Score + " " + Name + " Fail";
+                    lbxGrade.Items.Add(Score + " " + Name + " Fail");
+                }
+                else if (Score <= 60)
+                {
+                    lblGrade.Text = Score + " " + Name + " C";
+                    lbxGrade.Items.Add(Score + " " + Name + " C");
+                }
+                else if (Score <= 70)
+                {
+                    lblGrade.Text = Score + " " + Name + " B";
+                    lbxGrade.Items.Add(Score + " " + Name + " B");
+                }
+                else if (Score <= 80)
+                {
+                    lblGrade.Text = Score + " " + Name + " B+";
+                    lbxGrade.Items.Add(Score + " " + Name + " B+");
+                }
+                else if (Score <= 90)
+                {
+                    lblGrade.Text = Score + " " + Name + " A-";
+                    lbxGrade.Items.Add(Score + " " + Name + " A-");
+                }
+                else if (Score <= 100)
+                {
+                    lblGrade.Text = Score + " " + Name + " A";
+                    lbxGrade.Items.Add(Score + " " + Name + " A");
+                }
+                else if (Score > 100)
+                {
+                    lblGrade.Text = "Stop lying " + Name;
+                    lbxGrade.Items.Add("Stop lying " + Name);
+                }
+
+            }
+        }
+    }
+}
+
